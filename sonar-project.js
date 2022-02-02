2fe166f32d8ca19bd0363e538ae455f0a0111319
priyabratswain009 <86113527+priyabratswain009@users.noreply.github.com>
2022-02-02 23:14:04 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'ec2-3-21-134-176.us-east-2.compute.amazonaws.com:9000',
+  serverUrl: '3.143.226.139:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
